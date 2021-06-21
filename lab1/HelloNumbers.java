0be3c76cf0f55371a2dc87332e7794aa16f53388
AgachilyPaul <freegreen011@sina.com>
2021-06-21 21:56:33 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers 
+{
+    public static void main(String args[]) {
+        int i = 0;
+        int sum = 0;
+        while (i <= 9) {
+            sum = sum + i;
+            System.out.println(sum);
+            i++;
+        }
+    }
+}
